e2d2b3d866e91cb4488481fd5d8d01516fc10528
Diana.B <bek.ruhl@gmail.com>
2026-02-04 15:25:05 -0300

    csmbios en provider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,10 +5,15 @@ export const AuthContext = createContext()
 
 export const AUTH_TOKEN_KEY = 'auth_token'
 
+function decodeAuthToken (auth_token){
+    return jwtDecode(auth_token)
+}
+
 function AuthContextProvider ({children}){
 
-    const [isLogged, setIsLogged] = useState(false)
-    const [session, setSession] = useState(null)
+    const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
+    const [isLogged, setIsLogged] = useState(Boolean(auth_token))
+    const [session, setSession] = useState(auth_token ? decodeAuthToken(auth_token) : null)
 
     useEffect(
         /* 
@@ -16,12 +21,12 @@ function AuthContextProvider ({children}){
         GET / api / auth / validate-token (Authorization: bear auth-token) Te responde si el token es valido o no
         */
         () => {
-            const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
+/*             const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
             if(auth_token){
                 setIsLogged(true)
                 const session_decoded = jwtDecode(auth_token)
                 setSession(session_decoded)
-            }
+            } */
         },
         []
     )
