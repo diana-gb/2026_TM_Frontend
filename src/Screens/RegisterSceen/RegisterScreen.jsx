33e638c7cd2be7f946f0ecc76bd08f8e956262dc
Diana.B <bek.ruhl@gmail.com>
2026-02-21 18:30:39 -0300

    css

--- src/Screens/RegisterSceen/RegisterScreen.jsx
+++ src/Screens/RegisterSceen/RegisterScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
 import useRegister from '../../hooks/useRegister'
+import './RegisterScreen.css'
 
 const RegisterScreen = () => {
 
@@ -14,57 +15,71 @@ const RegisterScreen = () => {
     } = useRegister()
 
 return (
-    <div>
-        <h1>Registrate en la aplicacion</h1>
-        <form onSubmit={onSubmitForm}>
-            <div>
+    <div className='register_page'>
+        <div className='register_container'>
+            <div className='register_header'>
+                <h1>Registrate en la aplicacion</h1>
+            </div>
+
+            <div className='register_card'>
+
+        <form className='register_form' onSubmit={onSubmitForm}>
+            <div className='register_field'>
                 <label htmlFor="username">Nombre de usuario:</label>
                 <input 
                     type="text" 
                     id="username" 
                     name="username" 
+                    placeholder='Tu nombre'
                     value={form_state.username} 
                     onChange={onChangeFieldValue} 
                 />
             </div>
-            <div>
+
+            <div className='register_field'>
                 <label htmlFor="password">Contraseña:</label>
                 <input 
                     type="password" 
                     id="password" 
                     name="password" 
+                    placeholder='No pongas 1234'
                     value={form_state.password} 
                     onChange={onChangeFieldValue} 
                 />
             </div>
-            <div>
+            <div className='register_field'>
                 <label htmlFor="email">Email:</label>
                 <input 
                     type="email" 
                     id="email" 
-                    name="email" 
+                    name="email"
+                    placeholder='nombre@email.com' 
                     value={form_state.email} 
                     onChange={onChangeFieldValue}
                 />
             </div>
             {
-                error && <span style={{color: 'red'}}>{error.message}</span>
+                error && <span className='register_message_error'>{error.message}</span>
             }
             {
                 response 
                 && 
                 response.ok 
                 && 
-                <span style={{color: 'yellowgreen'}}>
+                <span className='register_message_succes'>
                     Usuario registrado exitosamente, te enviaremos un mail con instrucciones.
                 </span>
             }
-            <br/>
-            <button type="submit" disabled={loading}>Registrarse</button>
+            
+            <button type="submit" className='register_button' disabled={loading}>Registrarse</button>
         </form>
-        <span>
-            Ya tienes una cuenta? <Link to="/login">iniciar sesion</Link>
-        </span>
+            </div>
+
+            <div className='register_footer'>
+                <span>Ya tienes una cuenta? </span>
+                <Link to="/login">iniciar sesion</Link>
+            </div>
+        </div>
     </div>
 )
 }
