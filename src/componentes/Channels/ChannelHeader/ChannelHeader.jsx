2548b9f7b5c6b2759ba86c29079034e510c5868a
Diana.B <bek.ruhl@gmail.com>
2026-02-24 22:12:57 -0300

    mas css

--- src/componentes/Channels/ChannelHeader/ChannelHeader.jsx
+++ src/componentes/Channels/ChannelHeader/ChannelHeader.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 import './ChannelHeader.css'
+import ICONOS from "../../../Constantes/iconos"
 const ChannelHeader = () => {
     return (
         <div className="channels-header">
             <Link to="/home" className="back-link">
-                ← Volver
+                volver
             </Link>
             <h1>Canales</h1>
         </div>
