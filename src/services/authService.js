427d06dbca42a5be358a885ee6a11295a930176b
Diana.B <bek.ruhl@gmail.com>
2026-02-27 14:09:06 -0300

    -

--- src/services/authService.js
+++ src/services/authService.js
@@ -18,7 +18,7 @@ fetch recibe dos parametos:
  */
 
 const URL_API = import.meta.env.VITE_API_URL //'http://localhost:8080'
-console.log("URL_API:", URL_API);
+
 export async function login (email, password) {
 
     const response_http = await fetch(
